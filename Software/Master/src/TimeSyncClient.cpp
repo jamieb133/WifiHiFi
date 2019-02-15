521b5cdfcce0ec88f03e74d7a07c05b0db1838b8
JamieB133 <2127147b@student.gla.ac.uk>
2019-02-15 10:24:41 +0000

    timesync client receiving timestamps

--- Software/Master/src/TimeSyncClient.cpp
+++ Software/Master/src/TimeSyncClient.cpp
@@ -15,10 +15,10 @@ TimeSyncClient::TimeSyncClient(QObject* parent) : QObject(parent)
 }
 
 
-void TimeSyncClient::ProcessSyncRequest(int RxTimestamp)
+void TimeSyncClient::ProcessSyncRequest(quint32 RxSecs, quint32 RxNSecs)
 {
-    m_timeoffset = m_timestamp - RxTimestamp;
-    qDebug() << "OFFSET = " << m_timeoffset << endl;
+
+    
     GenerateDelayRequest();
 }
 
@@ -28,14 +28,19 @@ void TimeSyncClient::GenerateDelayRequest()
     DelayPacket.append(TIMESYNC_PREAMBLE);
     DelayPacket.append(DELAY_REQ);
     DelayPacket.append(m_timestamp);
-    socket->writeDatagram(DelayPacket, QHostAddress(TIMESYNC_MCAST), 1234);
+    socket->writeDatagram(DelayPacket, QHostAddress("192.168.0.255"), 1234);
     qDebug() << "SENT DELAY REQUEST" << endl;
+}
+
+void TimeSyncClient::ProcessDelayResponse(quint32 RxSecs, quint32 RxNSecs)
+{
+    
 }
 
 void TimeSyncClient::UDPCallback()
 {
     /* get current timestamp */
-    ReadTime(&m_timestamp);
+    //ReadTime(&m_timestamp);
 
     QByteArray Buffer;
     Buffer.resize(socket->pendingDatagramSize());
@@ -44,23 +49,28 @@ void TimeSyncClient::UDPCallback()
     quint16 senderPort;
     socket->readDatagram(Buffer.data(), Buffer.size(), &sender, &senderPort);
 
+    qDebug() << Buffer << endl;
+
     quint8 preamble = static_cast<quint8>(Buffer[0]);
     quint8 msgType = static_cast<quint8>(Buffer[1]);
-    quint16 upperTimestamp = static_cast<quint8>(Buffer[2]);
-    upperTimestamp = upperTimestamp << 8;
-    quint16 lowerTimestamp = static_cast<quint8>(Buffer[3]);
-    int RxTimestamp = upperTimestamp | lowerTimestamp;
+
 
     if (preamble == TIMESYNC_PREAMBLE)
     {
         switch (msgType)
         {
             case SYNC :         qDebug() << "Received sync message, processing" << endl;
-                                ProcessSyncRequest(RxTimestamp);
+                                RxSecs = 0;
+                                RxNSecs = UnpackTimestamp(Buffer, sizeof(Buffer));
+                                qDebug() << "Master timestamp = " << RxNSecs;
+                                ProcessSyncRequest(RxSecs, RxNSecs);
                                 break;
-            case DELAY_REQ:     qDebug() << "Received delay-request, not processing" << endl;
+            case DELAY_REQ:     //qDebug() << "Received delay-request, not processing" << endl;
                                 break;
             case DELAY_RESP:    qDebug() << "Received delay-response, processing" << endl;
+                                RxSecs = 0;
+                                RxNSecs = UnpackTimestamp(Buffer, sizeof(Buffer));
+
                                 break;
             default:            qDebug() << "ERROR: unrecognised time-sync message type" << endl;
                                 break;
@@ -69,8 +79,27 @@ void TimeSyncClient::UDPCallback()
 
 }
 
-void TimeSyncClient::ReadTime(int* ts)
+quint32 TimeSyncClient::UnpackTimestamp(QByteArray packet, int size)
 {
-    //*ts = (m_timer->interval) - (m_timer->remainingTime)
-    *ts = 40;
+    int index = size + 1;
+
+    quint8 timestampByte;
+    quint32 ts = 0;
+    quint32 val;
+    timestampByte = static_cast<quint8>(packet[6]);
+    val = timestampByte;
+    val = val << 24;
+    ts |= val; 
+    timestampByte = static_cast<quint8>(packet[7]);
+    val = timestampByte << 16;
+    ts |= val;
+    timestampByte = static_cast<quint8>(packet[8]);
+    val = timestampByte << 8;
+    ts |= val;
+    timestampByte = static_cast<quint8>(packet[9]);
+    val = timestampByte ;
+    ts |= val;
+    
+    return ts;
+    
 }
